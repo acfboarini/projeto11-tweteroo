061fe6bfae3105fed589ecfbac536410baaeaa06
Augusto <acfb.app@gmail.com>
2022-04-25 13:05:00 -0300

    v2: requisicao get para tweets feita

--- backend/index.js
+++ backend/index.js
@@ -16,9 +16,34 @@ app.post("/sign-up", (req, res) => {
 })
 
 app.get("/tweets", (req, res) => {
-    res.send(tweets);
+    const tweets2 = listaTweets(tweets);
+    res.send(tweets2);
+})
+
+app.post("/tweets", (req,res) => {
+    console.log(req.body);
+    tweets.push(req.body);
+    res.send('OK');
 })
 
 app.listen(5000, () => {
     console.log("a aplicacao esta rodando normalmente");
 });
+
+function listaTweets(tweets) {
+    let lista_tweets = tweets.map(tweet => {
+        return {
+            username: tweet.username,
+		    avatar: verificaAvatar(tweet.username),
+	        tweet: tweet.tweet
+        }
+    })
+    return lista_tweets;
+}
+
+function verificaAvatar(username) {
+    let user = usuarios.find(usuario => {
+        return usuario.username === username;
+    });
+    return user.avatar;
+}
